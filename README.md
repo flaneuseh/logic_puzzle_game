5c8ef171acb595a704f1d1e82ce1546876efffa2
Kaylah Facey <kaylah.facey@gmail.com>
2024-02-28 17:00:09 -0500

    update README and add missing dependencies; change Concede to Give Up

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
+Install dependencies:
+```
+npm install
+```
+
 To run 
 ```
 npx expo start
-```
-
-You may need to install dependencies:
-
-```
-npx expo install react-dom react-native-web @expo/metro-runtime
-npm install axios 
-npm i reactjs-popup
 ```
--- src/EndPuzzleButtons.js
+++ src/EndPuzzleButtons.js
@@ -2,50 +2,50 @@ import Popup from 'reactjs-popup';
 
 
 
-export default FinishButtons = ({giveUp, isCorrect, clearBoard, finish}) => {
+export default FinishButtons = ({ giveUp, isCorrect, clearBoard, finish }) => {
 
-    let CorrectPop = ({isCorrect, finish, clearBoard, close}) => {
-        let text  = ""
+    let CorrectPop = ({ isCorrect, finish, clearBoard, close }) => {
+        let text = ""
         let retry = ""
-        if(isCorrect()){
+        if (isCorrect()) {
             text = "You are correct"
-        }else{
+        } else {
             text = "You are not correct"
-    
-            retry = (<button onClick={() => {clearBoard(); close()}}>
-                    Retry
+
+            retry = (<button onClick={() => { clearBoard(); close() }}>
+                Retry
             </button>)
         }
-    
+
         return (<div>{text}
-           <div>
-            {retry}
-            <button onClick=
-                {() => {finish(); close()}}>
+            <div>
+                {retry}
+                <button onClick=
+                    {() => { finish(); close() }}>
                     Finish Puzzle
-            </button>
-    
-      
+                </button>
+
+
             </div>
         </div>)
     };
 
     return (<div>
-        <button onClick={giveUp}>Concede</button>
+        <button onClick={giveUp}>Give Up</button>
         <Popup trigger=
-                {<button> Check Solution </button>} 
-                modal nested>
-                {
-                    close => (
-                        <div className='modal'>
-                            <div className='content'>
-                                <CorrectPop isCorrect={isCorrect} clearBoard = {clearBoard} finish ={finish} close={() => close()}/>
-                            </div>
+            {<button> Check Solution </button>}
+            modal nested>
+            {
+                close => (
+                    <div className='modal'>
+                        <div className='content'>
+                            <CorrectPop isCorrect={isCorrect} clearBoard={clearBoard} finish={finish} close={() => close()} />
                         </div>
-                    )
-                }
-            </Popup>
-        <button onClick = {clearBoard}>Restart</button>
-        
+                    </div>
+                )
+            }
+        </Popup>
+        <button onClick={clearBoard}>Restart</button>
+
     </div>)
 }
