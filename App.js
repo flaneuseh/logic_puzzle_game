e443b66e4d176aa7f85be27eec2b50534217cfb1
Fiona Shyne <shynef@union.edu>
2024-03-13 12:13:38 -0400

    added school puzzles

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
   let [puzzle, setPuzzle] = useState(null); 
   let [i, setI] = useState(0)
 
-  let files = ["puzzles/trial_2_puzzle_0_0.json", "puzzles/trial_2_puzzle_1_3.json", "puzzles/trial_2_puzzle_2_6.json", "puzzles/trial_2_puzzle_8_4.json", "puzzles/trial_2_puzzle_9_2.json"]
+  let files = ["puzzles/trial4_puzzle0_0.json","puzzles/trial4_puzzle1_1.json", "puzzles/trial4_puzzle2_2.json", "puzzles/trial4_puzzle3_3.json", "puzzles/trial4_puzzle4_4.json",  "puzzles/trial4_puzzle5_5.json", "puzzles/trial4_puzzle6_6.json", "puzzles/trial4_puzzle7_7.json"] 
   shuffleArray(files)
       return (<PuzzleManager files={files} i={i} setI={setI}/> );
         
