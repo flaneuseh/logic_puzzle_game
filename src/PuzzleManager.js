6409b709149132d8be7218d94cfa1983530f57d3
Kaylah Facey <kaylah.facey@gmail.com>
2024-03-20 11:01:35 -0400

    light consistency editing

--- src/PuzzleManager.js
+++ src/PuzzleManager.js
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
 import axios from 'axios';
-import Puzzle from "./puzzle";
+import { useState } from "react";
+import ResponseRecorded from "./ResponseRecorded";
 import Category from "./categoryModel";
+import Puzzle from "./puzzle";
 import PuzzleModel from "./puzzleModel";
 import Survey from "./survey";
-import ResponseRecorded from "./ResponseRecorded";
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -15,68 +15,68 @@ function shuffleArray(array) {
     }
 }
 
-function createPuzzle(data){
+function createPuzzle(data) {
     let categories = []
-    for(cat in data.categories){
-      cat = data.categories[cat]
-      categories.push(new Category(cat.name, cat.entities))
-    } 
-  
-    return new PuzzleModel(categories, data.hints, data.solution, data.id) 
-  
-  }
+    for (cat in data.categories) {
+        cat = data.categories[cat]
+        categories.push(new Category(cat.name, cat.entities))
+    }
+
+    return new PuzzleModel(categories, data.hints, data.solution, data.id)
+
+}
 
 
-  function load(i, setI, setContent, files){
-    if(i >= files.length){
+function load(i, setI, setContent, files) {
+    if (i >= files.length) {
         setContent(<div>No more puzzles</div>);
-    }else{
+    } else {
         axios.get(files[i])
-        .then(response => {
-        let p = createPuzzle(response.data);
-        let time = new Date()
-        setI(i + 1);
-        setContent( <Puzzle p={p} time={time} concede = {()=>{startSurvey(p.num,  i, setI, setContent, files)}} finish = {()=> {startSurvey(p.num, i, setI, setContent, files)}}/>);
+            .then(response => {
+                let p = createPuzzle(response.data);
+                let time = new Date()
+                setI(i + 1);
+                setContent(<Puzzle p={p} time={time} concede={() => { startSurvey(p.num, i, setI, setContent, files) }} finish={() => { startSurvey(p.num, i, setI, setContent, files) }} />);
 
-        
-    });
 
-    
+            });
+
+
     }
 
 }
-function finish (setContent) {
-    setContent(<div>Thank you for your time, you may close this window now</div>)
+function finish(setContent) {
+    setContent(<div>Thank you for your time, you may close this window now.</div>)
 }
 
-function showRecordedScreen (setContent, i, setI, files){
-    setContent(<ResponseRecorded goToNextPuzzle={()=>{load( i + 1, setI, setContent, files)}} finish={() => {finish(setContent)}} morePuzzles={() => {return i + 1 < files.length}}/>) 
+function showRecordedScreen(setContent, i, setI, files) {
+    setContent(<ResponseRecorded goToNextPuzzle={() => { load(i + 1, setI, setContent, files) }} finish={() => { finish(setContent) }} morePuzzles={() => { return i + 1 < files.length }} />)
 }
 
-function startSurvey(puzzle, i, setI, setContent, files){
-    setContent(<Survey puzzleId={puzzle} submit={()=>showRecordedScreen(setContent, i , setI, files)}/>); 
+function startSurvey(puzzle, i, setI, setContent, files) {
+    setContent(<Survey puzzleId={puzzle} submit={() => showRecordedScreen(setContent, i, setI, files)} />);
     setI(i + 1)
 
-    }
+}
 
 
-export default PuzzleManager = ({files, i, setI}) => {
+export default PuzzleManager = ({ files, i, setI }) => {
     //shuffleArray(files);
     //let [i, setI] = useState(0);
-    let [content, setContent] = useState(<div>loading</div>); 
-    let [puzzle, setPuzzle] = useState(null); 
-    let [puzzleId, setPuzzleId] = useState(-1); 
-    
-        
-        
+    let [content, setContent] = useState(<div>loading</div>);
+    let [puzzle, setPuzzle] = useState(null);
+    let [puzzleId, setPuzzleId] = useState(-1);
 
-    if(i == 0){
+
+
+
+    if (i == 0) {
         load(i, setI, setContent, files);
 
     }
-    
 
-   
-    return(content)
+
+
+    return (content)
 
 }
