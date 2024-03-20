4d62caff6abf7bf8ef9c948194289883e13fad6f
Kaylah Facey <kaylah.facey@gmail.com>
2024-03-20 10:51:04 -0400

    allow vertical scroll of page

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
 
-export default Tutorial = ({imageFolder, numSlides, canSkip, startGame}) => {
-    let [idx, setIdx] = useState(1); 
+export default Tutorial = ({ imageFolder, numSlides, canSkip, startGame }) => {
+    let [idx, setIdx] = useState(1);
     let [proceed, setProceed] = useState("");
 
-    let increase = () =>{
-        if(idx < numSlides){
+    let increase = () => {
+        if (idx < numSlides) {
             setIdx(idx + 1);
         }
-        if(idx + 1 == canSkip ){
-            setProceed(<button className="startGame" onClick={startGame}>Start game</button>)
+        if (idx + 1 == canSkip) {
+            setProceed(<button className="startGame" onClick={startGame}>Start Puzzle</button>)
         }
-        
+
     }
 
-    let decrease = () =>{
-        if(idx > 1){
+    let decrease = () => {
+        if (idx > 1) {
             setIdx(idx - 1);
         }
     }
     return (<div className="tutorial">
         <div className="slide">
-        <img  src = {imageFolder + "/" + idx +".png"}/>
+            <img src={imageFolder + "/" + idx + ".png"} />
         </div>
-     
+
         <button className="backButton" onClick={decrease} disabled={idx <= 1}>Back</button>
-        <button className="nextButton"  onClick={increase}  disabled={idx >= numSlides}>Next</button>
+        <button className="nextButton" onClick={increase} disabled={idx >= numSlides}>Next</button>
         <div>
-        {proceed}
+            {proceed}
         </div>
-       
-        
+
+
     </div>)
 }
