c45c7cd3676a345adcbc143c49ad7c4611bc1d67
Kaylah Facey <kaylah.facey@gmail.com>
2024-05-02 11:38:44 -0400

    spelling and grammar

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 
-import { useEffect, useState } from 'react';
-import InitialSurvey from './src/InitialSurvey';
-import { getCurrentUser } from './src/Firestore/SignIn';
+import { useState } from 'react';
 import { addPuzzleSurvey, addSubject } from './src/Firestore/sendData';
+import InformedConsent from './src/InformedConsent';
+import InitialSurvey from './src/InitialSurvey';
 import PuzzleManager from './src/PuzzleManager';
 import Tutorial from './src/Tutorial';
-import InformedConsent from './src/InformedConsent';
 
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
 
@@ -28,18 +27,18 @@ function createPuzzle(data, setPuzzle) {
 
 }
 
-function getFiles(){
-  let columns = [0, 1, 3,5]
+function getFiles() {
+  let columns = [0, 1, 3, 5]
   let solutions = [12, 199, 352, 444]
   let files = []
 
-  shuffleArray(solutions) 
+  shuffleArray(solutions)
 
-  for(i in columns){
-    files.push("puzzles/puzzle_" + solutions[i] + "_"+ columns[i] + ".json")
+  for (i in columns) {
+    files.push("puzzles/puzzle_" + solutions[i] + "_" + columns[i] + ".json")
   }
 
-  return files 
+  return files
 
 
 }
@@ -60,9 +59,9 @@ export default function App() {
   let files = getFiles()
   //let files = ["puzzles/example.json"]
 
-  let consent = <div className='parent'><InformedConsent consent={() => setMode("survey")}/></div>
+  let consent = <div className='parent'><InformedConsent consent={() => setMode("survey")} /></div>
   let tutorial = <Tutorial imageFolder="tutorialSlides" numSlides={29} canSkip={12} startGame={() => { startGame() }} />
-  let puzzleManager = <PuzzleManager files={files} i={i} setI={setI} pid={pid} postSurvey={addPuzzleSurvey}/>
+  let puzzleManager = <PuzzleManager files={files} i={i} setI={setI} pid={pid} postSurvey={addPuzzleSurvey} />
 
   let startGame = () => {
     setMode("puzzle")
@@ -73,31 +72,31 @@ export default function App() {
 
   let submitInitalSurvey = (logicPuz, gridPuzz) => {
     setMode("tutorial");
-    addSubject(logicPuz, gridPuzz) 
+    addSubject(logicPuz, gridPuzz)
   }
-  
- 
+
+
   shuffleArray(files)
 
-  if (mode == "consent"){
+  if (mode == "consent") {
     return (consent)
   }
   else if (mode == "survey") {
     return (
-     
+
       <InitialSurvey postAnswers={submitInitalSurvey} />
     )
   } else if (mode == "tutorial") {
     return (<div className='parent'>{tutorial}</div>)
   } else {
     return (<div className='parent'>
-        <div className='codeBanner'>
-          <div>
-          You completetion code is 4XPAFDSASD <br/> 
-              You may enter this at anytime 
-          </div>
-             
+      <div className='codeBanner'>
+        <div>
+          Your completion code is 4XPAFDSASD <br />
+          You may enter this at anytime
         </div>
+
+      </div>
       {puzzleManager}</div>)
   }
 
