5af5530de04154bfd1a03585422529d167affe67
Fiona Shyne <shynef@union.edu>
2024-06-10 16:33:30 -0400

    updated consent pdf

--- src/InformedConsent.js
+++ src/InformedConsent.js
@@ -1,22 +1,25 @@
 import * as React from 'react';
+import { RenderPage, RenderPageProps, Viewer } from '@react-pdf-viewer/core';
 
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import FailedConsent from './FailedConsent';
 
 const ConsentViewer =({ consent }) => {
     let [rejected, setRejected] = React.useState(false);
-    const docs = "./17-10-07--Consent_Prolific_UnsignedWebBased--2024-05-29-1.png"
+    const docs = [
+        { uri: "./Consent_linked.pdf" }, // Local File
+      ];
+
     if (!rejected){
       return (
       
         <div className='consent'>
             <div>
-            <img src={docs}/>
-              <div>
-                <button className="consentButtonLeft" onClick={consent}>I Agree</button>
-                <button className="consentButtonRight" onClick={() => {setRejected(true)}}>I do not agree</button>
-              </div>
+            <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />
             </div>
-             
              
         </div>
        )
